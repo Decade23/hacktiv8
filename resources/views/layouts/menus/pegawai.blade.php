5582ec25f14ba7204d08e2a1322c03d68b0824ed
Dedi Fardiyanto <dedifardiyanto@yahoo.co.id>
2019-08-30 15:29:01 +0700

    no message

---
+++ resources/views/layouts/menus/pegawai.blade.php
@@ -0,0 +1,22 @@
+<ul class="sidenav sidenav-collapsible leftside-navigation collapsible sidenav-fixed menu-shadow" id="slide-out" data-menu="menu-navigation" data-collapsible="menu-accordion">
+    <li class="bold"><a class="collapsible-header waves-effect waves-cyan " href="#"><i class="material-icons">settings_input_svideo</i><span class="menu-title" data-i18n="">Dashboard</span><span class="badge badge pill orange float-right mr-10">3</span></a>
+      <div class="collapsible-body">
+        <ul class="collapsible collapsible-sub" data-collapsible="accordion">
+          <li><a class="collapsible-body" href="{{ route('pegawai.index') }}" data-i18n=""><i class="material-icons">radio_button_unchecked</i><span>Data Pegawai</span></a>
+          </li>
+          <li><a class="collapsible-body" href="{{ route('pegawai.index') }}" data-i18n=""><i class="material-icons">radio_button_unchecked</i><span>Riwayat Pendidikan</span></a>
+          </li>
+          <li><a class="collapsible-body" href="dashboard-ecommerce.html" data-i18n=""><i class="material-icons">radio_button_unchecked</i><span>Kepegawaian</span></a>
+          </li>
+          <li><a class="collapsible-body" href="dashboard-analytics.html" data-i18n=""><i class="material-icons">radio_button_unchecked</i><span>Mutasi</span></a>
+          </li>
+          
+          <li><a class="collapsible-body" href="dashboard-analytics.html" data-i18n=""><i class="material-icons">radio_button_unchecked</i><span>Report</span></a>
+          </li>
+        </ul>
+      </div>
+    </li>
+   
+    <li class="bold"><a class="waves-effect waves-cyan " href="https://pixinvent.ticksy.com/"><i class="material-icons">help_outline</i><span class="menu-title" data-i18n="">Support</span></a>
+    </li>
+  </ul>
